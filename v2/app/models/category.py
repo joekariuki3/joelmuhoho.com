49be2ba98fc3ca5b1d5026f89edb1a7213f30c3e
joekariuki3 <joekariuki3@gmail.com>
2024-10-23 20:07:06 +0300

    category model to categories table.

---
+++ v2/app/models/category.py
@@ -0,0 +1,9 @@
+from app import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+    # relationships
+    projects = db.relationship('Project', backref='categories', lazy=True)
