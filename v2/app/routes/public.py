66bcf42f92ab43e39c7dcd1ecba286e7acb2bab8
joekariuki3 <joekariuki3@gmail.com>
2024-10-24 15:24:45 +0300

    added typings and refactored the routs functions to use class to interact with the database while performing crud functionality

--- v2/app/routes/public.py
+++ v2/app/routes/public.py
@@ -6,15 +6,32 @@ from sqlalchemy.exc import OperationalError
 public = Blueprint('public', __name__)
 
 @public.route('/')
-def home():
+def home() -> 'flask.Response':
+    """
+    The home page of the public site.
+    Returns a rendered template of the home page.
+    """
     try:
-        categories = Category.query.all()
-        full_stack_category = categories[0]
-        front_end_category = categories[1]
-        email_category = categories[2]
-        return render_template('index.html',full_stack_category=full_stack_category, front_end_category=front_end_category, email_category=email_category)
-    except OperationalError as e:
-        return handle_db_error(e)  # Call the custom error handler directly
+        # Get the categories
+        full_stack_category: Category = Category.get_by_id(1)
+        front_end_category: Category = Category.get_by_id(2)
+        email_category: Category = Category.get_by_id(3)
 
-def handle_db_error(error):
+        # Render the template
+        return render_template('index.html',
+                               full_stack_category=full_stack_category,
+                               front_end_category=front_end_category,
+                               email_category=email_category)
+    except OperationalError as e:
+        # If there is an operational error, call the custom error handler
+        return handle_db_error(e)
+
+def handle_db_error(error: OperationalError) -> 'tuple[flask.Response, int]':
+    """
+    A custom error handler to handle database operational errors.
+    Args:
+        error: The error that is raised when there is an operational error.
+    Returns:
+        A rendered template of the error page with a 500 status code.
+    """
     return render_template('error.html', message="Oops! It seems there's an issue with the database. Please check if the tables are set up correctly."), 500
