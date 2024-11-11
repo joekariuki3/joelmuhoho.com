285cb924db08e178df2fc59a87e7378355e9ef14
joekariuki3 <joekariuki3@gmail.com>
2024-11-11 23:59:27 +0300

    in RegisterForm i added username(optional) field and role_id field, when initiating the form i added edit_mode which defaults to false and when set to true (meaning form is used to edit user details), password, confirm password fields are optional, and username field is required

--- v2/app/forms/register_form.py
+++ v2/app/forms/register_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
-from app.utils import PasswordConstants, EmailConstants, NameConstants
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, Optional
+from app.utils import PasswordConstants, EmailConstants, NameConstants, RoleConstants
 
 class RegisterForm(FlaskForm):
     """Register form"""
@@ -11,11 +11,17 @@ class RegisterForm(FlaskForm):
     last_name = StringField('Your Last Name',
                             validators=[DataRequired(message=NameConstants.LAST_NAME_REQUIRED_MESSAGE)],
                             render_kw={'placeholder': "Last Name"})
+    username = StringField('Your Username',
+                           validators=[Optional()],
+                           render_kw={'placeholder': NameConstants.USERNAME_PLACEHOLDER})
     email = StringField('Your Email',
                         validators=[
                             DataRequired(message=EmailConstants.EMAIL_REQUIRED_MESSAGE),
                             Email(message=EmailConstants.EMAIL_ERROR_MESSAGE)],
                         render_kw={'placeholder': "name@mail.com"})
+    role_id = SelectField('Role', choices=[],
+                          validators=[DataRequired(message=RoleConstants.ROLE_REQUIRED_MESSAGE)],
+                          render_kw={'placeholder': RoleConstants.ROLE_PLACEHOLDER})
     password = PasswordField('Password',
                              validators=[
                                  Length(
@@ -34,3 +40,12 @@ class RegisterForm(FlaskForm):
                                          ],
                                      render_kw={'placeholder': "••••••••"})
     submit = SubmitField('Create an account')
+
+    def __init__(self, edit_mode=False, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+
+        # If edit_mode is True, make password fields optional
+        if edit_mode:
+            self.password.validators = [Optional()]
+            self.confirm_password.validators = [Optional()]
+            self.username.validators = [DataRequired(message=NameConstants.USERNAME_REQUIRED_MESSAGE)]
