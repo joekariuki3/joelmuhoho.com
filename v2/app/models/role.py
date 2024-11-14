6005a6a006ba889b7f6b8027aadeb15b634640c3
joekariuki3 <joekariuki3@gmail.com>
2024-11-14 18:15:40 +0300

    if root user is one logged in show all the roles including root and default

--- v2/app/models/role.py
+++ v2/app/models/role.py
@@ -1,6 +1,7 @@
 from .basemodel import BaseModel, db
 from app.utils import RoleConstants
 from app.models import User
+from flask_login import current_user
 
 class Role(BaseModel):
     __tablename__ = 'roles'
@@ -13,7 +14,10 @@ class Role(BaseModel):
     # get roles except root
     @classmethod
     def get_all(cls):
+        if current_user and current_user.is_root():
+            return cls.query.all()
         return cls.query.filter(Role.name != RoleConstants.ROOT, Role.name != RoleConstants.DEFAULT).all()
+
 
     # when deleting a role set users with roles that is being deleted to role default
     def delete(self):
