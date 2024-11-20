c3de35b6682ec0d89a92dd0de68eb27a1987be9d
joekariuki3 <joekariuki3@gmail.com>
2024-11-20 13:23:42 +0300

    return the root user to index page to use their dynamic information in home page

--- v2/app/routes/public.py
+++ v2/app/routes/public.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
-from app.models import Category
+from app.models import Category, User
 from sqlalchemy.exc import OperationalError
+from config import Config
 
 public = Blueprint('public', __name__)
 
@@ -15,11 +16,13 @@ def home() -> 'flask.Response':
         full_stack_category: Category = Category.get_by_like_name('Full Stack')
         front_end_category: Category = Category.get_by_like_name('Front End')
         email_category: Category = Category.get_by_like_name('email')
+        root_user = User.get_by_email(Config.ROOT_EMAIL)
         # Render the template
-        return render_template('index.html',
+        return render_template('public/index.html',
                                full_stack_category=full_stack_category,
                                front_end_category=front_end_category,
-                               email_category=email_category)
+                               email_category=email_category,
+                               user=root_user)
     except OperationalError as e:
         # If there is an operational error, call the custom error handler
         return handle_db_error(e)
@@ -32,4 +35,9 @@ def handle_db_error(error: OperationalError) -> 'tuple[flask.Response, int]':
     Returns:
         A rendered template of the error page with a 500 status code.
     """
-    return render_template('error.html', message="Oops! It seems there's an issue with the database. Please check if the tables are set up correctly."), 500
+    message="Oops! It seems there's an issue with the database. Please check if the tables are set up correctly."
+    return render_template('public/error.html', message=message), 500
+
+@public.route('/resume')
+def resume() -> 'flask.Response':
+    return render_template('public/resume.html')
