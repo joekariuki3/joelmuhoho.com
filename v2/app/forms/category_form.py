8fd5910b039833cb3e9124b5552cb5ac63b2b394
joekariuki3 <joekariuki3@gmail.com>
2024-11-20 15:05:19 +0300

    use select field to restrict categories to be added in the system

--- v2/app/forms/category_form.py
+++ v2/app/forms/category_form.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import SelectField, SubmitField
 from wtforms.validators import DataRequired
 from app.utils import CategoryConstants
 
 
 class CategoryForm(FlaskForm):
-    name = StringField('Category Name',
+    name = SelectField('Category Name',
+                       choices=[],
                         validators=[DataRequired(message=CategoryConstants.CATEGORY_NAME_REQUIRED_MESSAGE)],
                         render_kw={'placeholder': CategoryConstants.CATEGORY_NAME_PLACEHOLDER})
     submit = SubmitField("Submit")
